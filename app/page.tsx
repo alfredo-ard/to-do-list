bb7f0c7cdd04670a344ab435fd99d15c206e1080
alfredo <alfredo67316@gmail.com>
2025-02-22 21:36:48 +0700

    add code

--- app/page.tsx
+++ app/page.tsx
@@ -66,78 +66,86 @@ export default function Home() {
     }, 0);
 
     return (
-        <div className="h-[100vh] bg-slate-200 font-[inter]">
-            <nav className="font-[serif] bg-teal-500 px-5 py-2 border-red-300 mb-10">
-                <h1 className="font-semibold text-3xl tracking-[3px]">list</h1>
-            </nav>
-            <div className="w-[35%]  my-5 mx-auto">
-                <form action="" className="flex align-middle justify-between">
-                    <input
-                        name="item"
-                        type="text"
-                        className="w-[90%] px-2 py-1 text-lg border border-sky-300 rounded focus:outline-none focus:ring-sky-300 focus:ring-2"
-                        placeholder="What do you wanna buy.."
-                        onChange={handleChange}
-                        value={input.item}
-                    />
-                    <button
-                        type="button"
-                        onClick={handleClick}
-                        className="text-slate-600 cursor-pointer border border-sky-600 bg-sky-300 text-xs rounded-full hover:bg-sky-600 hover:text-slate-100 shadow-md p-1 ml-3 size-10"
+        <div>
+            <div className="min-h-[90vh] bg-slate-200 font-[inter] relative">
+                <nav className="font-[serif] bg-teal-500 px-5 py-2 border-red-300 mb-10">
+                    <h1 className="font-semibold text-3xl tracking-[3px]">
+                        list
+                    </h1>
+                </nav>
+                <div className="w-[35%]  my-5 mx-auto">
+                    <form
+                        action=""
+                        className="flex align-middle justify-between"
                     >
-                        <AddIcon />
-                    </button>
-                </form>
-            </div>
+                        <input
+                            name="item"
+                            type="text"
+                            className="w-[90%] px-2 py-1 text-lg border border-sky-300 rounded focus:outline-none focus:ring-sky-300 focus:ring-2"
+                            placeholder="What do you wanna buy.."
+                            onChange={handleChange}
+                            value={input.item}
+                        />
+                        <button
+                            type="button"
+                            onClick={handleClick}
+                            className="text-slate-600 cursor-pointer border border-sky-600 bg-sky-300 text-xs rounded-full hover:bg-sky-600 hover:text-slate-100 shadow-md p-1 ml-3 size-10"
+                        >
+                            <AddIcon />
+                        </button>
+                    </form>
+                </div>
 
-            {lists.map((list, index) => {
-                return (
-                    <div
-                        key={index}
-                        className="w-[35%]  my-5 mx-auto bg-sky-200 px-3 py-2 shadow-md  rounded flex align-middle justify-between"
-                    >
-                        <h2 className="text-xl text-slate-600">{list.item}</h2>
+                {lists.map((list, index) => {
+                    return (
+                        <div
+                            key={index}
+                            className="w-[35%]  my-5 mx-auto bg-sky-200 px-3 py-2 shadow-md  rounded flex align-middle justify-between"
+                        >
+                            <h2 className="text-xl text-slate-600">
+                                {list.item}
+                            </h2>
 
-                        <div className="flex align-middle justify-evenly gap-2">
-                            <button
-                                type="button"
-                                onClick={() => {
-                                    handleMin(index);
-                                }}
-                                className="text-slate-600 cursor-pointer border border-sky-600 bg-sky-300 text-xs rounded-full hover:bg-sky-600 hover:text-slate-100 shadow-md p-1"
-                            >
-                                <RemoveIcon />
-                            </button>
+                            <div className="flex align-middle justify-evenly gap-2">
+                                <button
+                                    type="button"
+                                    onClick={() => {
+                                        handleMin(index);
+                                    }}
+                                    className="text-slate-600 cursor-pointer border border-sky-600 bg-sky-300 text-xs rounded-full hover:bg-sky-600 hover:text-slate-100 shadow-md p-1"
+                                >
+                                    <RemoveIcon />
+                                </button>
 
-                            <p className="text-xl text-slate-600">
-                                {list.count}
-                            </p>
+                                <p className="text-xl text-slate-600">
+                                    {list.count}
+                                </p>
 
-                            <button
-                                type="button"
-                                onClick={() => {
-                                    handlePlus(index);
-                                }}
-                                className="text-slate-600 cursor-pointer border border-sky-600 bg-sky-300 text-xs rounded-full hover:bg-sky-600 hover:text-slate-100 shadow-md p-1"
-                            >
-                                <AddIcon />
-                            </button>
+                                <button
+                                    type="button"
+                                    onClick={() => {
+                                        handlePlus(index);
+                                    }}
+                                    className="text-slate-600 cursor-pointer border border-sky-600 bg-sky-300 text-xs rounded-full hover:bg-sky-600 hover:text-slate-100 shadow-md p-1"
+                                >
+                                    <AddIcon />
+                                </button>
 
-                            <button
-                                type="button"
-                                onClick={() => {
-                                    handleDelete(index);
-                                }}
-                                className="text-slate-600 cursor-pointer border border-sky-600 bg-sky-300 text-xs rounded-full hover:bg-sky-600 hover:text-slate-100 shadow-md p-1 ml-3"
-                            >
-                                <DeleteIcon />
-                            </button>
+                                <button
+                                    type="button"
+                                    onClick={() => {
+                                        handleDelete(index);
+                                    }}
+                                    className="text-slate-600 cursor-pointer border border-sky-600 bg-sky-300 text-xs rounded-full hover:bg-sky-600 hover:text-slate-100 shadow-md p-1 ml-3"
+                                >
+                                    <DeleteIcon />
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                );
-            })}
-
-            <div className="w-full bg-teal-500 absolute bottom-0 h-20 shadow text-center p-5">
+                    );
+                })}
+            </div>
+            <div className="w-full bg-teal-500 h-20 shadow text-center p-5">
                 <p className="text-xl mx-auto text-slate-600">
                     total item: {countItem}
                 </p>
